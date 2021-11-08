3790650048c19c4ff1a18625d3a1d54ffdb70f59
LucaOcchio99 <occhionorelli.luca99@gmail.com>
2021-11-08 17:32:36 +0100

    end snack 1

--- Snack 1/js/main.js
+++ Snack 1/js/main.js
@@ -53,3 +53,105 @@ console.log(giocatore_basket.nome_giocatore, giocatore_basket.cognome_giocatore,
 
 
 
+//4. Creare un array di 10 giocatori di basket, con le regole sopra indicate
+
+const dieci_giocatori = [{
+        codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+        nome_giocatore : 'Luca',
+        cognome_giocatore : 'Occhionorelli',
+        eta_giocatore : '21',
+        media_punti : cifre_random(0, 50),
+        successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Vinny',
+    cognome_giocatore : 'Carbutti',
+    eta_giocatore : '12',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Frangggeee',
+    cognome_giocatore : 'Cozzolino',
+    eta_giocatore : '33',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Mbavingenz',
+    cognome_giocatore : 'Scaringi',
+    eta_giocatore : '21',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Giuen',
+    cognome_giocatore : 'Terrone',
+    eta_giocatore : '21',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Mimè',
+    cognome_giocatore : 'Occhio',
+    eta_giocatore : '21',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Tromolo',
+    cognome_giocatore : 'Barese',
+    eta_giocatore : '21',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Samueeeeee',
+    cognome_giocatore : 'Cnigh',
+    eta_giocatore : '21',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Lucia',
+    cognome_giocatore : 'Latti',
+    eta_giocatore : '45',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+},
+{
+    codice_giocatore : cifre_random(100, 899) + generateRandomString(3),
+    nome_giocatore : 'Giancarlo',
+    cognome_giocatore : 'Badoui',
+    eta_giocatore : '56',
+    media_punti : cifre_random(0, 50),
+    successo_tiri : cifre_random(0, 100),
+}];
+
+console.log(dieci_giocatori);
+
+/* Creare un nuovo array con i giocatori che hanno una
+ media di punti superiore a 35 e la percentuale di 
+successo per i tiri da 3 punti superiore all’80%.*/
+
+const punti_maggiori = dieci_giocatori.filter((giocatore) => {
+
+    return giocatore.media_punti > 34;
+})
+
+console.log(punti_maggiori);
+
+const tiri_80 = dieci_giocatori.filter((tiro_giocatore) => {
+
+    return tiro_giocatore.successo_tiri > 80;
+})
+
+console.log(tiri_80);
